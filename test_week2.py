8ce55938760ce6e67c5931a87e157931cb3c144f
Francesco Richard <francesco.richard.02@gmail.com>
2025-02-14 10:36:49 +0100

    update test file

--- test_week2.py
+++ test_week2.py
@@ -2,25 +2,31 @@
 """
 import preclass_assignment.functions as fxn
 
-
 def test_greet(capsys):
     """Check that the greet function prints as expected"""
     # given
     name = 'world'  # who should we greet?
     # when
-    fxn.greet(name)  # greet them
+    fxn.printing_word(name)  # greet them
     captured = capsys.readouterr()  # capture what would have been printed to screen
     # then
-    assert captured.out == 'Hello, world!\n'  # check that the greeting was what we expect
+    assert captured.out == 'Hello, world\n'  # check that the greeting was what we expect
 
 
 
 def test_goldilocks(capsys):
     """Check goldilocks returns expected output"""
     # given
+    test_cases = [(139, 'Too small!!'),
+                  (140, 'Just right :)'),
+                  (150, 'Just right :)'),
+                  (151, 'Too big!!')]
     # when
+    for length, message in test_cases:
+        fxn.length_bed(length)  # actual output
+        captured = capsys.readouterr()  # capture what would have been printed to screen
     # then
-    assert False  # TODO! Update the contents of this function so it correctly tests goldilocks
+        assert captured.out.strip() == message 
 
 
 def test_square_list():
@@ -37,15 +43,21 @@ def test_square_list():
 def test_fibonacci_stop():
     """Check fibonacci functions works as expected."""
     # given
-    # given
+    inp = 30 # test input to function
+    exp_out = [1, 1, 2, 3, 5, 8, 13, 21]  # expected output
     # when
+    out = fxn.fibonacci_stop(inp)
     # then
-    assert False  # TODO! Update the contents of this function so it correctly tests fibonacci_stop
+    assert exp_out == out 
 
 
 def test_clean_pitch():
     """Check clean_pitch works as expected."""
     # given
+    inp_angles = [-1, 2, 6, 95]  # test input to function
+    inp_status = [1, 0, 0, 0]  # test input to function
+    exp_out = [-999, 2, 6, 95]
     # when
+    out = fxn.clean_pitch(inp_angles, inp_status)  # actual output
     # then
-    assert False  # TODO! Update the contents of this function so it correctly tests clean_pitch
+    assert  exp_out == out #Update the contents of this function so it correctly tests clean_pitch
